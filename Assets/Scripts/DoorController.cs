46278cf664b15abb8719a79d878eae511a07ce25
lee-forde <46717325+lee-forde@users.noreply.github.com>
2019-02-07 20:20:54 +0000

    Wip

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    public float moveStep;
+    public float openYPos;
+    public float closedYPos;
+    public float doorDelay;
+
+
+    void Awake()
+    {
+        //Assuming door is closed
+        closedYPos = transform.position.y;
+    }
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        LevelManager.instance.OnDoorTriggerEnter(other);
+    }
+     void OnTriggerExit2D(Collider2D other)
+    {
+        LevelManager.instance.OnDoorTriggerExit(other);
+    }
+
+    public void open()
+    {
+        StartCoroutine("moveDoor", true);
+    }
+    public void close()
+    {
+        StartCoroutine("moveDoor", false);
+
+    }
+
+    private IEnumerator moveDoor(bool openDirection)
+    {
+        if (openDirection == true)
+        {
+            Vector2 currentPos = transform.position;
+
+            while (currentPos.y < openYPos)
+            {
+                currentPos.y += moveStep;
+                transform.position = currentPos;
+                yield return new WaitForSeconds(doorDelay);
+
+            }
+        }
+        else
+        {
+            Vector2 currentPos = transform.position;
+            while (currentPos.y > closedYPos)
+            {
+                currentPos.y -= moveStep;
+                transform.position = currentPos;
+                yield return new WaitForSeconds(doorDelay);
+            }
+        }
+           
+             
+        
+    }
+}
+
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+    public DoorController theDoor;
+    public bool doorOpen = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnDoorTriggerEnter(Collider2D other)
+    {
+        Debug.Log("LevelManager:OnDoorTriggerEnter");
+
+        theDoor.open ();
+    }
+
+    public void OnDoorTriggerExit(Collider2D other)
+    {
+        Debug.Log("LevelManager:OnDoorTriggerExit");
+
+        theDoor.close ();
+    }
+}
